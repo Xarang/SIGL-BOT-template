b7aa9d4fb0be67d5291b4448e94f1110522e156c
Victor Coatalem <victor.coatalem@epita.fr>
2021-09-29 21:01:59 +0200

    admin command

--- src/main.py
+++ src/main.py
@@ -7,21 +7,18 @@ from dotenv import load_dotenv
 
 from name import handle_name
 from count import handle_count
+from admin import handle_admin
 
 load_dotenv()
 intents = discord.Intents.default()
 intents.members = True
-client = discord.Client(intents=intents)
 
 
 bot = commands.Bot(command_prefix='!', intents=intents)
 
-@commands.command(name='name')
-async def on_message(ctx):
-    await ctx.channel.send(ctx.author)
-
 bot.add_command(handle_name)
 bot.add_command(handle_count)
+bot.add_command(handle_admin)
 bot.run(os.getenv('DISCORD_BOT_TOKEN'))
 
 #client.run(os.getenv('DISCORD_BOT_TOKEN'))
